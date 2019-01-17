e30a591b359e610d3cb1f06c3964a22a6150a1dd
Jun Ito <jun_ito@voyagegroup.com>
2019-01-17 20:02:13 +0900

    Update: nullまわりの挙動を確認

--- src/js/my_mod.ts
+++ src/js/my_mod.ts
@@ -2,4 +2,16 @@ export default class MyMod {
   message (): string {
     return 'hello'
   }
+
+  extract_number (text: string): number | null {
+    const res = Array.prototype.filter.call(text, (char: string) => {
+      return '0123456789'.indexOf(char) !== -1
+    }).join('')
+
+    if (res.length === 0) {
+      return null
+    }
+
+    return parseFloat(res)
+  }
 }
--- test/my_mod_test.ts
+++ test/my_mod_test.ts
@@ -1,11 +1,23 @@
-import * as chai from 'chai'
-
+import { assert } from 'chai'
 import MyMod from '../src/js/my_mod'
 
 const m = new MyMod()
 
 describe('MyMod Instance', () => {
   it('create correct message', () => {
-    chai.assert.equal(m.message(), 'hello')
+    assert.equal(m.message(), 'hello')
+  })
+
+  it('extract number correctly', () => {
+    const cases = [
+      { case: '123', expected: 123 },
+      { case: 'abc', expected: null },
+      { case: 'a1b2', expected: 12 }
+    ]
+
+    for (const c of cases) {
+      const actual = m.extract_number(c.case)
+      assert.equal(actual, c.expected)
+    }
   })
 })
