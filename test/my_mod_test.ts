493f28f841f980e277147d39d24bbd8469d0c6be
Jun Ito <jun_ito@voyagegroup.com>
2019-01-17 19:13:09 +0900

    Add: test case

---
+++ test/my_mod_test.ts
@@ -0,0 +1,11 @@
+import * as chai from 'chai'
+
+import MyMod from "../src/js/my_mod";
+
+const m = new MyMod()
+
+describe("MyMod Instance", () => {
+    it("create correct message", () => {
+        chai.assert.equal(m.message(), "hello")
+    })
+})
