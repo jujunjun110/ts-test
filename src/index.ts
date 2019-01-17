71194793173cc66fa802aa15b2168c859c8847bc
Jun Ito <jun_ito@voyagegroup.com>
2019-01-17 23:56:18 +0900

    Update: use 2 spaces

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,7 @@ import MyMod from './js/my_mod'
 import './scss/style.scss'
 
 async function main() {
-  console.log(123)
-
   const m = new MyMod()
-  console.log(m.message())
-
   const res = await m.fetch_url('http://google.com')
   console.log(res)
 }
