4261803a53b01af5bd85f4c78f967b900b334ab5
Jun Ito <jun_ito@voyagegroup.com>
2019-01-20 20:01:15 +0900

    Update: 所要時間の大幅な違いを確かめるテストコードを追加

--- test/my_mod_test.ts
+++ test/my_mod_test.ts
@@ -33,5 +33,27 @@ describe('MyMod Instance', () => {
       assert.equal(actual, c.expected)
     }
   })
+
+  it('is fast', () => {
+    const cases = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    for (const n of cases) {
+      const start1 = Date.now()
+      const res1 = m.fib(n)
+      const end1 = Date.now()
+      const elapsed1 = (end1 - start1)
+
+      const start2 = Date.now()
+      const res2 = m.fib2(n)
+      const end2 = Date.now()
+      const elapsed2 = (end2 - start2)
+
+      assert.equal(res1, res2)
+
+      console.log(`case: ${n}`)
+      console.log(`logic01: ${elapsed1.toPrecision(5)} required`)
+      console.log(`logic02: ${elapsed2.toPrecision(5)} required`)
+    }
+  })
+
 })
 
