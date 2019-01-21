0535aa0dcc3bb95a8191773ddaef103cc18f1d75
Jun Ito <jun_ito@voyagegroup.com>
2019-01-22 02:24:21 +0900

    Update: lint

--- test/my_mod_test.ts
+++ test/my_mod_test.ts
@@ -21,13 +21,13 @@ describe('MyMod Instance', () => {
     }
   })
 
-  it("calculate fib number", () => {
+  it('calculate fib number', () => {
     const cases = [
       { case: 0, expected: 0 },
       { case: 1, expected: 1 },
       { case: 2, expected: 1 },
       { case: 10, expected: 55 },
-      { case: 100, expected: 354224848179262015075 },
+      { case: 100, expected: 354224848179262015075 }
     ]
 
     for (const c of cases) {
@@ -35,4 +35,3 @@ describe('MyMod Instance', () => {
     }
   })
 })
-
